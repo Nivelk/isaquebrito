92069e7dbe5742093242f775095d72133f0e6453
42143 <aslan-k@hotmail.com>
2017-06-29 03:25:34 -0300

    atualização themes/

--- wp-content/themes/isaquebritos/header.php
+++ wp-content/themes/isaquebritos/header.php
@@ -9,5 +9,22 @@
         <link rel="shortcut icon" href="<?php echo get_template_directory_uri() . '/imagens/favicon.png'; ?>"/> 
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
         <?php wp_head(); ?>
+        <!--CDN Bootsstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous"/>
+        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
     </head>
     <body <?php body_class(); ?> >
+        <header>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col">
+                        <img src="<?php echo get_template_directory_uri().'/imagens/logo.png';?>"/>
+                        <p>ISAQUE DE BRITO SANTOS</p>
+                    </div>
+                    <div class="col"></div>
+                    <div class="col"></div>
+                </div>
+            </div>
+        </header>
