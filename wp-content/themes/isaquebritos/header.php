bc209dde86d13ac6d8c6ef22ae3120d7774e305a
42143 <aslan-k@hotmail.com>
2017-06-29 03:35:16 -0300

    atualição themes

--- wp-content/themes/isaquebritos/header.php
+++ wp-content/themes/isaquebritos/header.php
@@ -14,6 +14,7 @@
         <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+        <title><?php wp_title( '|', true, 'right'); ?><?php bloginfo('name'); ?> </title>
     </head>
     <body <?php body_class(); ?> >
         <header>
@@ -22,6 +23,7 @@
                     <div class="col">
                         <img src="<?php echo get_template_directory_uri().'/imagens/logo.png';?>"/>
                         <p>ISAQUE DE BRITO SANTOS</p>
+                        <p>Corretor de Imóveis, CRECI: 115.193</p>
                     </div>
                     <div class="col"></div>
                     <div class="col"></div>
