0a34793c8b3595b2b25326bac06e023160a4beaa
42143 <aslan-k@hotmail.com>
2017-07-03 10:52:52 -0300

    atualização themes/

--- wp-content/themes/isaquebritos/content.php
+++ wp-content/themes/isaquebritos/content.php
@@ -6,15 +6,18 @@
 ?>
 <div class="container">
     <div class="row">
-            <?php if( $query->have_posts() ) : while ($query->have_posts() ) : $query->the_post(); ?>
-        
-                <?php get_template_part('post', get_post_format() ) ?>
-        
-            <?php endwhile; else:  ?>
-        
-                <h4><?php _e('Ups...') ?></h4>
-                <p><?php _e('Desculpe, nenhum pagina foi encontrado.') ?></p>
-        
-            <?php endif; ?>
+        <?php 
+            if($query->have_posts()):
+                while($query->have_posts()):
+                    $query->the_post();
+                    get_template_part('post', get_post_format());
+                endwhile;
+            else:
+        ?>
+            <h4><?php _e('Ups...') ?></h4>
+            <p><?php _e('Desculpe, nenhum pagina foi encontrado.') ?></p>
+        <?php
+            endif;
+        ?>
     </div>
 </div>
--- wp-content/themes/isaquebritos/post.php
+++ wp-content/themes/isaquebritos/post.php
@@ -1,62 +1,40 @@
 <?php
-/**
- * Standard Post Format
- */
 ?>
-
-<!-- Standard -->
-<article <?php post_class(); ?> style="border:solid 1px red;">
-	<div class="feature-image">
-		<a href="<?php the_permalink(); ?>">
-            
-			<?php
-                $image_attributes = wp_get_attachment_image_src( $attachment->ID );
-                    if ( $image_attributes ) : ?>
-                         <img src="<?php echo $image_attributes[0]; ?>" width="<?php echo $image_attributes[1]; ?>" height="<?php echo $image_attributes[2]; ?>" />
-                    <?php endif; ?>
-            <?php
-	           // Renders the image for the posts
-	          // $image_url = ansimuz_post_image();
-	           //if(!$image_url) 
-		          //return;
-	          // $image_url = ansimuz_build_image($image_url, 643);
-            ?>		
-            <img src="<?php echo $image_url; ?>" alt="<?php the_title(); ?>" />
-		</a>
-	</div>
-	<div class="box cf">
-		<?php
-            /*
-            * Date include on all the posts
-            *
-            */
-        ?>
-        <div class="entry-date">
-	       <div class="number"><?php the_time('d') ?></div><div class="month"><?php the_time('M') ?></div>
+<div class="col-3">
+    <div class="row">
+        <div <?php post_class();?>>
+            <div class="col foto">
+                <a href="<?php the_permalink();?>">
+                    <?php
+                        $imagenUrl = '';
+                        $imagenId = get_post_thumbnail_id();
+                        $imagenUrl = wp_get_attachment_image_src($imagenId);
+                    
+                        if($imagenUrl):
+                    ?>
+                            <img src="<?php echo $imagenUrl[0];?>" alt="<?php the_title();?>" />
+                    <?php
+                        else:
+                    ?>
+                            <img src="<?php echo get_template_directory_uri().'/images/anexo.png';?>"/>
+                    <?php
+                        endif;
+                    ?>
+                </a>    
+            </div>
+            <div class="box">
+                <center>
+                    <a href="<?php the_permalink(); ?>" class="post-heading" >
+                        <?php the_title();?>
+                    </a>
+                </center>
+                    <?php the_excerpt();?>
+                <center>
+                    <p>
+                        <a href="<?php the_permalink(); ?>" class="btn btn-outline-primary "><?php _e('Veja Mais'); ?></a>
+                    </p>
+                </center>
+            </div>
         </div>
-		
-		<div class="excerpt">
-			<a href="<?php the_permalink(); ?>" class="post-heading" ><?php the_title() ?></a>
-			<?php the_excerpt() ?>
-			
-			<p><a href="<?php the_permalink(); ?>" class="learnmore"><?php _e('Veja Mais'); ?></a></p>
-		</div>
-		
-		<?php 
-            /*
-            * Meta data included on all of the posts
-            *
-            */
-        ?>
-
-        <div class="meta">
-            <span class="format"><?php _e('Posted by'); ?> </span>
-            <span class="user"><?php the_author_link() ?></span>
-            <span class="comments"><?php comments_number('0', '1', '%') ?> <?php _e('Comments'); ?> </span>
-            <span class="tags"><?php the_tags('',', ', '') ?></span>
-        </div>
-			
-	</div>
-</article>
-<!-- ENDS  Standard -->
-
+    </div>    
+</div>
