584b2d94169f2f4c8ea82d1834a66839fad3cdd7
42143 <aslan-k@hotmail.com>
2017-08-01 02:18:07 -0300

    atualização do site isaquebritos

---
+++ wp-content/themes/alante/footer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id="main-core".
+ *
+ * @package ThinkUpThemes
+ */
+?>
+
+	
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
